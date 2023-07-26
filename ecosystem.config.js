feff26a307483c10c094bfcdc71fd0c216ef7b86
CGH <tt878429@gmail.com>
2023-07-26 13:50:04 +0900

    Chore: pm2 cluster모드로 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,10 @@ module.exports = {
       name: "project",
       script: "npm",
       args: "run start",
-      instances: 2,
+      instances: 2, // 클러스터 모드에서 생성할 pm2 인스턴스 개수
+      exec_mode: "cluster",
       autorestart: true,
-      watch: true,
+      watch: true, // 파일 변경 감지
       env: {
         PORT: 3000,
         NODE_ENV: "development",
