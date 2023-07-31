c9dc91ba16246c3de52a805682e20764290224db
CGH <tt878429@gmail.com>
2023-07-31 13:01:26 +0900

    :lipstick: Design: Header에 style 적용

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -1,38 +1,39 @@
 import Image from "next/image";
-import Link from "next/link";
 
 import dot from "/public/dot.svg";
 import logo from "/public/logo.svg";
 
-import { HeaderWrapper, LoginBtn, Navigation } from "./Header.styles";
+import {
+  HeaderInnerWrapper,
+  HeaderOuterWrapper,
+  LoginBtn,
+  Navigation,
+} from "./Header.styles";
+import NavLink from "./NavLink";
 
 const Header = () => {
   return (
-    <HeaderWrapper>
-      <Image src={logo} alt="chagok logo" />
-      <Navigation>
-        <ul>
-          <li>
-            <Link href="/">
-              <span>홈</span>
-            </Link>
-            <Image src={dot} alt="middle dot" />
-          </li>
-          <li>
-            <Link href="/contests">
-              <span>해커톤 / 공모전</span>
-            </Link>
-            <Image src={dot} alt="middle dot" />
-          </li>
-          <li>
-            <Link href="/projects">
-              <span>스터디 / 프로젝트</span>
-            </Link>
-          </li>
-        </ul>
-      </Navigation>
-      <LoginBtn type="button">로그인</LoginBtn>
-    </HeaderWrapper>
+    <HeaderOuterWrapper>
+      <HeaderInnerWrapper>
+        <Image src={logo} alt="chagok logo" />
+        <Navigation>
+          <ul>
+            <li>
+              <NavLink href="/">홈</NavLink>
+              <Image src={dot} alt="middle dot" />
+            </li>
+            <li>
+              <NavLink href="/contests">해커톤</NavLink>
+              <Image src={dot} alt="middle dot" />
+            </li>
+            <li>
+              <NavLink href="/projects">스터디 / 프로젝트</NavLink>
+            </li>
+          </ul>
+        </Navigation>
+        <LoginBtn type="button">로그인</LoginBtn>
+      </HeaderInnerWrapper>
+    </HeaderOuterWrapper>
   );
 };
 
