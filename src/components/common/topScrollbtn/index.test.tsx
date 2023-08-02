0fa0fad69ae10ef1207f129987ae96fdec245099
CGH <tt878429@gmail.com>
2023-08-02 22:01:22 +0900

    :white_check_mark: Test: TopScrollBtn 테스팅 추가

---
+++ src/components/common/topScrollbtn/index.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import TopScrollBtn from ".";
+
+test("버튼을 클릭하면 scroll을 위로 올리는 함수가 호출된다.", async () => {
+  const user = userEvent.setup();
+  global.scrollTo = jest.fn();
+
+  render(<TopScrollBtn />);
+
+  const scrollBtn = screen.getByRole("button");
+  await user.click(scrollBtn);
+
+  expect(global.scrollTo).toBeCalled();
+});
