d8df0c5089a2bd2ef25d5c60a622e66308f37cc3
최규호 (Choi Gyu Ho) <68717963+gyuhoBest@users.noreply.github.com>
2023-08-02 23:44:39 +0900

    💄 Design: 랜딩페이지 프로젝트, 스터디 캐러셀 구현 (#14)
    * :lipstick: Design: 프로젝트 / 스터디 캐러셀 구현
    
    * :lipstick: Design: 레이아웃에 Hr태그 추가
    
    * :sparkles: Feat: TopScroll 버튼 구현
    
    * :package: Docs: Swiper 버전 다운그레이드
    
    Swiper 버전 10의 type이 typescript 4.9.5와 맞지 않아서 에러가 발생합니다.
    
    * :lipstick: Design: 프로젝트 분류 태그를 button에서 span으로 변경
    
    * :white_check_mark: Test: TopScrollBtn 테스팅 추가

---
+++ src/components/common/topScrollbtn/index.styles.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const CircleBtn = styled.button`
+  display: block;
+
+  width: 4.5rem;
+  height: 4.5rem;
+
+  background-color: #ff6b00;
+  border: 0;
+  border-radius: 100%;
+
+  margin: 0 2rem 0 auto;
+
+  cursor: pointer;
+`;
---
+++ src/components/home/recommendation/index.tsx
@@ -0,0 +1,25 @@
+import Hr from "components/common/Hr";
+import { H2, P, Section, ShowMore } from "components/home/index.styles";
+import Image from "next/image";
+
+import arrow from "/public/showMore.svg";
+
+const Recommendation = () => {
+  return (
+    <>
+      <Section>
+        <div className="section-title">
+          <H2>김철수님을 위한 추천 과제</H2>
+          <P>철수님의 취향에 맞춘 추천 프로젝트들을 구경해보세요!</P>
+          <ShowMore href="projects">
+            <span>더 보기</span>
+            <Image src={arrow} alt="show more" />
+          </ShowMore>
+        </div>
+      </Section>
+      <Hr />
+    </>
+  );
+};
+
+export default Recommendation;
