d4435d43ed0c92677bf7d86cc428d650fbe3c5a4
aggie97 <4ggie1997@gmail.com>
2023-08-02 18:38:59 +0900

    ✨ Feat: 공통 hr 컴포넌트 생성
    공통 hr 컴포넌트 스타일링 및 생성

---
+++ src/components/common/Hr.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const StyledHr = styled.div`
+  border-top: 7px solid #f2f2f2;
+  margin-block: 1rem;
+`;
+
+const Hr = () => {
+  return <StyledHr />;
+};
+
+export default Hr;
