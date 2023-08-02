92067028f8802f3458799eb24f125c78a57a72ca
aggie97 <4ggie1997@gmail.com>
2023-08-03 08:13:23 +0900

    💄 Design: 공통 hr 디자인 수정
    페이지 양옆 공백에 상관없이 전체 viewport를 차지하도록 스타일 변경

--- src/components/common/Hr.tsx
+++ src/components/common/Hr.tsx
@@ -1,12 +1,24 @@
 import styled from "@emotion/styled";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
 const StyledHr = styled.div`
   border-top: 7px solid #f2f2f2;
   margin-block: 1rem;
+  width: 1vw;
+  transform: scaleX(100);
 `;
 
 const Hr = () => {
-  return <StyledHr />;
+  return (
+    <Wrapper>
+      <StyledHr />
+    </Wrapper>
+  );
 };
 
 export default Hr;
