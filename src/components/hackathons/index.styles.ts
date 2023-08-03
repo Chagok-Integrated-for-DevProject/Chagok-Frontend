f86de64a429db3f25f5a33cf1582f60e99178df3
aggie97 <4ggie1997@gmail.com>
2023-08-03 21:11:16 +0900

    💄 Design: 해커톤 목록 페이지 레이아웃 구현
    HackathonPage : 해커톤 목록 페이지
      - 추천 section - 구분선 - 목록 section 순으로 분리 후 pages/hackathons/[id]에
        바인딩
    
      - 추천 section
        - 캐러셀 미적용 상태 (구현 예정)
    
      - 목록 section
        - select arrow button 커스텀 스타일 적용
        - 길이 9의 임의 배열 생성 후 해커톤 카드 grid로 레이아웃 구현
    
    /public : public 폴더
      - Mocking을 위한 더미 이미지 2 개 추가
    
    공통 컴포넌트
    ScrabButton
      - 공통 컴포넌트 스크랩 버튼 구현
      - svg 최적화 회의 후 결정
    
    HackathonPageCard
      - 해커톤 목록 페이지에 사용되는 해커톤 카드 구현
      - 테스트용 더미 데이터 삽입
    
    MainPageCard
      - 홈페이지에 사용되는 해커톤 카드 구현
      - 테스트용 더미 데이터 삽입
      - gird-areas로 내부 레이아웃 구현

---
+++ src/components/hackathons/index.styles.ts
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Section = styled.section`
+  padding: 0 2.5rem;
+  margin-top: 6rem;
+`;
+
+export const H2 = styled.h2`
+  font-size: 1.75rem;
+  font-weight: 700;
+  line-height: 2.625rem;
+`;
+
+export const H3 = styled.h3`
+  font-size: 1.125rem;
+  font-weight: 400;
+  line-height: 1.6875rem;
+`;
