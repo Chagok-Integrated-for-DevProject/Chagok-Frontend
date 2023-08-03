a0592ca191b28f01b31b73444bf54209a9a5a67a
CGH <tt878429@gmail.com>
2023-08-03 20:00:33 +0900

    :lipstick: Design: ProjectCarousel 디자인 수정

--- src/components/home/projects/Carousel.styles.ts
+++ src/components/home/projects/Carousel.styles.ts
@@ -1,21 +1,21 @@
 import styled from "@emotion/styled";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-export const CarouselWrapper = styled.div`
-  padding: 0 0.6rem;
-  margin: 2rem 0 3rem -0.6rem;
-  overflow: hidden;
-`;
-
-export const CustomSwiperWrapper = styled(Swiper)`
-  overflow: visible;
+export const CustomSwiper = styled(Swiper)`
+  padding: 1.875rem 0.6rem;
+  border-radius: 1rem;
+  margin: 0 0 3rem -0.6rem;
 `;
 
 export const CustomSwiperSlide = styled(SwiperSlide)`
   position: relative;
-  z-index: 1;
+  display: block;
 
-  box-shadow: 1px 1px 1rem -0.6rem;
+  width: 420px !important;
+  padding: 2.5rem 2.2rem;
+  border-radius: 0.8rem;
+
+  box-shadow: 1px 1px 3.125rem -1.5rem;
   border-radius: 1rem;
   margin: 0.2rem 0.1rem 0.875rem;
 `;
