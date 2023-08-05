ee1c60acae22d89f34efe8693740a1169df29cd3
CGH <tt878429@gmail.com>
2023-08-05 19:01:56 +0900

    :package: Chore: pm2 설정 변경

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,8 +1,5 @@
  echo "> after install"
 
  cd /home/ubuntu/chagok
- sudo rm -r node_modules
- sudo npm install
  sudo npm run build
- sudo pm2 kill;
- sudo pm2 start "npm run start" --name chagok-fe
+ sudo pm2 reload chagok-fe
