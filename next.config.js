a26f39f05bcd24d5a4852c980de86672c777995d
CGH <tt878429@gmail.com>
2023-08-05 16:53:35 +0900

    :package: Chore: bundleAnalyzer 및 svg사용을 위한 세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer({ ...nextConfig });
