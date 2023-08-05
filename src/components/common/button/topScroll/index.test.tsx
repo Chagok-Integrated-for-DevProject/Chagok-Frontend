3c2c52627257e6b167e94d3ba468b2de4540f67f
CGH <tt878429@gmail.com>
2023-08-05 19:22:27 +0900

    :art: Styles: 테스팅 파일 오타 수정

--- src/components/common/button/topScroll/index.test.tsx
+++ src/components/common/button/topScroll/index.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import TopScrollBtn from "src/components/common/button/button/topScroll";
+
+import TopScrollBtn from ".";
 
 test("버튼을 클릭하면 scroll을 위로 올리는 함수가 호출된다.", async () => {
   const user = userEvent.setup();
