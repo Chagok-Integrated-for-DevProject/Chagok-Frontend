abe158679df8ff7a6a73ecebe29eedd9bbda6593
aggie97 <4ggie1997@gmail.com>
2023-08-06 23:42:02 +0900

    💄 Design: 해커톤 상세 페이지 레이아웃 구현
    해커톤 상세 페이지 요약, 원문, 댓글로 섹션 생성 후 구분
    
    요약 부분 퍼블리싱
    
    공통 컴포넌트 ScrabButton 아이콘 그림자로 인한 위치 불안정을 padding 값으로 고정

--- src/components/common/button/scrab/index.styles.ts
+++ src/components/common/button/scrab/index.styles.ts
@@ -3,6 +3,6 @@ import styled from "@emotion/styled";
 export const StyledScrabButton = styled.button`
   border: none;
   cursor: pointer;
-  padding: 0;
-  margin-left: 1rem;
+  padding: 0.5rem 0 0 0.5rem;
+  margin: 0.5rem 0 0 0.5rem;
 `;
