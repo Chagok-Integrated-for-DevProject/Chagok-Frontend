87571c47381f7964eeacdc4400d72bc39eaf0c63
CGH <tt878429@gmail.com>
2023-08-08 14:41:08 +0900

    ":sparkles: Feat: SearchInput(검색창) 구현
    SKillFilter 및 resetStyles의 css속성을 SearchInput에 맞게 변경

--- src/components/projects/search/SearchInput.styles.ts
+++ src/components/projects/search/SearchInput.styles.ts
@@ -2,5 +2,41 @@ import styled from "@emotion/styled";
 import { palette } from "styles/palette";
 
 export const SearchInputWrapper = styled.div`
+  position: relative;
   background-color: ${palette.bgGray300};
+  border-radius: 0.625rem;
+  overflow: hidden;
+`;
+
+export const Input = styled.input`
+  position: relative;
+  z-index: 1;
+
+  width: 24.8125rem;
+  height: 3.75rem;
+  max-height: 60px;
+  background-color: transparent;
+
+  padding: 1.25rem 1.5625rem;
+  border: 0;
+
+  font-size: 0.875rem;
+  line-height: 1.1875rem;
+`;
+
+export const Label = styled.label`
+  position: absolute;
+  top: 1.2rem;
+  left: 1.5rem;
+
+  display: block;
+
+  font-size: 0.875rem;
+  line-height: 1.1875rem;
+  color: ${palette.fontGray100};
+`;
+
+export const Placeholder = styled.span`
+  display: inline-block;
+  margin-left: 0.5rem;
 `;
