7301b98a19ff1e65f7d75df8f14d6b8031c467fd
aggie97 <4ggie1997@gmail.com>
2023-08-09 20:18:00 +0900

    💄 Design: 해커톤 상세 페이지 해커톤 원문 스타일링
    원문(Original) 섹션 레이아웃 및 스타일링
    
    XSS 공격 방지용 isormophic-dompurify(SSR + CSR) 설치 및 적용

---
+++ src/components/hackathons/detail/original/index.styles.ts
@@ -0,0 +1,38 @@
+import styled from "@emotion/styled";
+import Image from "next/image";
+
+interface IArrowStyle {
+  isOpen: boolean;
+}
+
+export const Details = styled.details``;
+
+export const Summary = styled.summary`
+  max-width: 8rem;
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  ::-webkit-details-marker,
+  ::marker {
+    display: none;
+  }
+
+  cursor: pointer;
+
+  span {
+    display: inline-block;
+    text-align: center;
+    width: 5rem;
+  }
+`;
+
+export const Arrow = styled(Image)`
+  transform: rotate(
+    ${({ isOpen }: IArrowStyle) => (isOpen ? "180deg" : "0deg")}
+  );
+`;
+
+export const CrawlingDataBox = styled.div``;
