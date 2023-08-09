fa817938e0021f5b0150d4e6d87dfdf893638c91
CGH <tt878429@gmail.com>
2023-08-09 18:34:03 +0900

    :sparkles: Feat: MainBanner를 vertical Carousel로 변경

--- src/components/common/mainBanner/index.tsx
+++ src/components/common/mainBanner/index.tsx
@@ -6,22 +6,53 @@ import {
   SecondCard,
   ThirdCard,
 } from "components/common/card/mainBanner";
+import Image from "next/image";
+import { Autoplay, Navigation, Pagination } from "swiper";
 
-import { CustomSwiper, CustomSwiperSlide } from "./index.styles";
+import next from "/public/banner/arrow/next.svg";
+import prev from "/public/banner/arrow/prev.svg";
+
+import {
+  CustomSwiper,
+  CustomSwiperSlide,
+  MainBannerWrawpper,
+  NextBtn,
+  PrevBtn,
+} from "./index.styles";
 
 const MainBanner = () => {
   return (
-    <CustomSwiper>
-      <CustomSwiperSlide>
-        <FirstCard />
-      </CustomSwiperSlide>
-      <CustomSwiperSlide>
-        <SecondCard />
-      </CustomSwiperSlide>
-      <CustomSwiperSlide>
-        <ThirdCard />
-      </CustomSwiperSlide>
-    </CustomSwiper>
+    <MainBannerWrawpper>
+      <CustomSwiper
+        direction={"vertical"}
+        navigation={{
+          prevEl: ".prev",
+          nextEl: ".next",
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        loop
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        modules={[Navigation, Pagination, Autoplay]}
+      >
+        <PrevBtn className="prev">
+          <Image src={prev} alt="prev arrow" />
+        </PrevBtn>
+        <CustomSwiperSlide>
+          <FirstCard />
+        </CustomSwiperSlide>
+        <CustomSwiperSlide>
+          <SecondCard />
+        </CustomSwiperSlide>
+        <CustomSwiperSlide>
+          <ThirdCard />
+        </CustomSwiperSlide>
+        <NextBtn className="next">
+          <Image src={next} alt="next arrow" />
+        </NextBtn>
+      </CustomSwiper>
+    </MainBannerWrawpper>
   );
 };
 
