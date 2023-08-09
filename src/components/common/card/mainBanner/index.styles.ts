86a8b3fb156c21a00a8ad7be8da0b2856890b1a3
CGH <tt878429@gmail.com>
2023-08-09 21:35:13 +0900

    :lipstick: Design: BannerImage를 픽셀 수 4배(좌우 2배) 올린 것으로 교체

--- src/components/common/card/mainBanner/index.styles.ts
+++ src/components/common/card/mainBanner/index.styles.ts
@@ -1,20 +1,47 @@
 import styled from "@emotion/styled";
+import Image from "next/image";
 import { breakPoints } from "styles/breakPoints";
 import { palette } from "styles/palette";
 
 export const CardWrapper = styled.div`
+  position: relative;
+
   width: 100%;
   height: 100%;
 `;
 
+export const CustomImg = styled(Image)`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  width: 55.625rem;
+  height: 26.125rem;
+
+  @media ${breakPoints.md} {
+    width: 42rem;
+    height: 20rem;
+  }
+
+  @media ${breakPoints.sm} {
+    width: 32rem;
+    height: 14rem;
+  }
+
+  @media ${breakPoints.xs} {
+    width: 100vw;
+    height: 50vw;
+  }
+`;
+
 export const TextWrapper = styled.div`
-  position: relative;
+  position: absolute;
   z-index: 1;
 
-  width: 100%;
   height: 100%;
 
-  margin: 8% auto auto 7%;
+  top: 23%;
+  left: 7%;
 `;
 
 export const H2 = styled.h2`
@@ -23,17 +50,14 @@ export const H2 = styled.h2`
   font-size: 2rem;
   line-height: 3rem;
 
-  /*@media ${breakPoints.md} {
-    font-size: 1.8rem;
-    line-height: 2.8rem;
-  }*/
-
   @media ${breakPoints.sm} {
-    font-size: 1.4rem;
+    font-size: 1.6rem;
     line-height: 2.4rem;
   }
 
   @media ${breakPoints.xs} {
+    font-size: 1.4rem;
+    line-height: 2.2rem;
   }
 `;
 
