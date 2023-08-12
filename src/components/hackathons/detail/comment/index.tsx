0879e82cbb4aa4236a95fb34dd2d474b71d33e5a
aggie97 <4ggie1997@gmail.com>
2023-08-12 20:43:07 +0900

    💄 Design: 해커톤 상세 페이지 댓글 섹션 구현
    해커톤 상세 페이지 댓글 생성 및 댓글 조회 레이아웃 구현, 스타일링 적용
    말풍선 이미지 chat.svg 추가
    더미 댓글 데이터 추가 (삭제 예정)

--- src/components/hackathons/detail/comment/index.tsx
+++ src/components/hackathons/detail/comment/index.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
+
+import ChatSVG from "/public/utils/chat.svg";
+
 import * as S from "./index.styles";
 import CommentItem from "./item";
 import NewComment from "./register";
 
-export interface Comment {
+export interface IComment {
   commentId: number;
   content: string;
   createdDate: string;
@@ -14,7 +18,7 @@ export interface Comment {
 
 const Comment = () => {
   const dummyDate = new Date().toLocaleDateString();
-  const commentData: Comment[] = [
+  const commentData: IComment[] = [
     {
       commentId: 1,
       content:
@@ -22,19 +26,44 @@ const Comment = () => {
       createdDate: dummyDate,
       deleted: false,
       linkedComment: [null],
-      memberNickName: "테스트",
+      memberNickName: "테스트1",
+      parentId: 0,
+    },
+    {
+      commentId: 2,
+      content:
+        "댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트 댓글 내용 테스트",
+      createdDate: dummyDate,
+      deleted: false,
+      linkedComment: [null],
+      memberNickName: "테스트2",
       parentId: 0,
     },
   ];
   return (
     <section>
-      <S.CommentCount>모집글 {2}개</S.CommentCount>
+      <S.CommentCount>
+        <Image width={24} height={24} src={ChatSVG} alt="말풍선" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="48"
+          height="48"
+          viewBox="0 0 48 48"
+          fill="none"
+        >
+          <path
+            d="M4 44V7C4 6.2 4.3 5.5 4.9 4.9C5.5 4.3 6.2 4 7 4H41C41.8 4 42.5 4.3 43.1 4.9C43.7 5.5 44 6.2 44 7V33C44 33.8 43.7 34.5 43.1 35.1C42.5 35.7 41.8 36 41 36H12L4 44ZM10.7 33H41V7H7V37L10.7 33Z"
+            fill="black"
+          />
+        </svg>
+        <span>모집글 {2}개</span>
+      </S.CommentCount>
       <NewComment />
-      <ul>
+      <S.CommentListBox>
         {commentData.map((comment) => (
           <CommentItem key={comment.commentId} comment={comment} />
         ))}
-      </ul>
+      </S.CommentListBox>
     </section>
   );
 };
