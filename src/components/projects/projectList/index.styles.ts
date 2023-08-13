994705ffe0d698393c9ff565c5342f89297ec1b6
최규호 (Choi Gyu Ho) <68717963+gyuhoBest@users.noreply.github.com>
2023-08-13 12:21:04 +0900

    :lipstick: Design: ProjectList Grid레이아웃 추가

---
+++ src/components/projects/projectList/index.styles.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+import { palette } from "styles/palette";
+
+export const ProjectListGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 23.4375rem);
+  grid-template-rows: repeat(3, 25.9375rem);
+  gap: 2.375rem;
+
+  margin: 6.4375rem 0;
+`;
+
+export const GridItem = styled.div`
+  padding: 2.3125rem;
+
+  border-radius: 0.625rem;
+  background: ${palette.white};
+  box-shadow: 0px 1px 50px 0px rgba(0, 0, 0, 0.1);
+`;
--- src/components/projects/projectList/index.tsx
+++ src/components/projects/projectList/index.tsx
@@ -1,5 +1,24 @@
+import ProjectCard from "components/common/card/projects";
+
+import { GridItem, ProjectListGrid } from "./index.styles";
+
+// import Pagination from "components/common/pagination";
+
 const ProjectList = () => {
-  return <div>ProjectList</div>;
+  const Cards = new Array(9).fill(null).map((e) => {
+    e = <ProjectCard />;
+    return e;
+  });
+
+  return (
+    <>
+      <ProjectListGrid>
+        {Cards.map((e, i) => (
+          <GridItem key={i}>{e}</GridItem>
+        ))}
+      </ProjectListGrid>
+    </>
+  );
 };
 
 export default ProjectList;
