3a38436cc04f5dd4dd8c2eb82c333fd8f50e64ac
CGH <tt878429@gmail.com>
2023-08-14 11:49:19 +0900

    :package: Chore: react-query devtools 설치

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Layout from "components/common/layout";
 import type { AppProps } from "next/app";
 import { useState } from "react";
@@ -23,6 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
           <Component {...pageProps} />
         </Layout>
       </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
