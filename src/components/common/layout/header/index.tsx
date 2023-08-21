72a3d929bfb570f4cbb493cdd8329d8683b6d679
aggie97 <4ggie1997@gmail.com>
2023-08-21 19:52:26 +0900

    ✨ Feat: Common Modal 구현
    공통 스타일을 가진 Modal을 `createPortal`로 구현 및 스타일링
    `Portal`이 열릴 위치 생성 (_document.tsx)
    Modal 제어를 위한 useModal Hook 구현
    닫기 버튼 svg 파일 추가
    
    마이페이지 디자인 변경에 따른 스타일 수정
    IDE 내에서 직관적인 타입 확인을 위한 breakPoints와 palette의 `interface`를 `type alias`로 타입 네이밍 방식 변경

--- src/components/common/layout/header/index.tsx
+++ src/components/common/layout/header/index.tsx
@@ -1,3 +1,5 @@
+import Modal from "components/common/modal";
+import useModal from "lib/hooks/useModal";
 import Image from "next/image";
 
 import logo from "/public/logo.svg";
@@ -12,30 +14,38 @@ import {
 import NavLink from "./NavLink";
 
 const Header = () => {
+  const { isModalOpen, openModal, closeModal } = useModal();
   return (
-    <HeaderOuterWrapper>
-      <HeaderInnerWrapper>
-        <Image src={logo} alt="chagok logo" />
-        <Navigation>
-          <ul>
-            <li>
-              <NavLink href="/">홈</NavLink>
-              <Image src={dot} alt="middle dot" />
-            </li>
-            <li>
-              <NavLink href="/hackathons">해커톤</NavLink>
-              <Image src={dot} alt="middle dot" />
-            </li>
-            <li>
-              <NavLink href="/projects" query={{ purpose: "study" }}>
-                스터디 / 프로젝트
-              </NavLink>
-            </li>
-          </ul>
-        </Navigation>
-        <LoginBtn type="button">로그인</LoginBtn>
-      </HeaderInnerWrapper>
-    </HeaderOuterWrapper>
+    <>
+      <HeaderOuterWrapper>
+        <HeaderInnerWrapper>
+          <Image src={logo} alt="chagok logo" />
+          <Navigation>
+            <ul>
+              <li>
+                <NavLink href="/">홈</NavLink>
+                <Image src={dot} alt="middle dot" />
+              </li>
+              <li>
+                <NavLink href="/hackathons">해커톤</NavLink>
+                <Image src={dot} alt="middle dot" />
+              </li>
+              <li>
+                <NavLink href="/projects" query={{ purpose: "study" }}>
+                  스터디 / 프로젝트
+                </NavLink>
+              </li>
+            </ul>
+          </Navigation>
+          <LoginBtn type="button" onClick={openModal}>
+            로그인
+          </LoginBtn>
+        </HeaderInnerWrapper>
+      </HeaderOuterWrapper>
+      <Modal isOpen={isModalOpen} onCloseModal={closeModal}>
+        <></>
+      </Modal>
+    </>
   );
 };
 
