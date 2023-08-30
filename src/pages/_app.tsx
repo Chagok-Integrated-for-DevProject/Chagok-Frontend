b73f32ce6335fcc86451973e6d0d95f7ef41847f
CGH <tt878429@gmail.com>
2023-08-30 19:18:28 +0900

    :bug: Fix: googleClientID유출된 모든 커밋 내역 삭제 후, _app.tsx재작성

---
+++ src/pages/_app.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Global } from "@emotion/react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import ErrorBoundary from "components/common/error/ErrorBoundary";
+import Layout from "components/common/layout";
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { useState } from "react";
+import { resetStyles } from "styles/resetStyles";
+
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  import("../lib/mocks");
+}
+
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  const kakaoInit = () => {
+    window.Kakao.init(process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID as string);
+    window.Kakao.isInitialized();
+  };
+
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            suspense: true,
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Global styles={resetStyles} />
+        <ErrorBoundary>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+          <Script
+            src="https://t1.kakaocdn.net/kakao_js_sdk/2.3.0/kakao.min.js"
+            integrity="sha384-70k0rrouSYPWJt7q9rSTKpiTfX6USlMYjZUtr1Du+9o4cGvhPAWxngdtVZDdErlh"
+            crossOrigin="anonymous"
+            onLoad={kakaoInit}
+          />
+        </ErrorBoundary>
+      </Hydrate>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
